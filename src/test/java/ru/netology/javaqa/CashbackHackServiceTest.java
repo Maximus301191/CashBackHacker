953d9f71239a98032dca7705f2d543e16ba6c964
Maximus301191 <maxalut301191@gmail.com>
2025-04-13 13:44:28 +0300

    Second commit

---
+++ src/test/java/ru/netology/javaqa/CashbackHackServiceTest.java
@@ -0,0 +1,50 @@
+package ru.netology.javaqa;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+
+public class CashbackHackServiceTest {
+    CashbackHackService cashbackHackService = new CashbackHackService();
+
+    @Test
+    public void ShouldBuy1USD() {
+        int amount = 1;
+        int expected = 999;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals (actual,expected);
+    }
+
+    @Test
+    public void ShouldBuy999USD() {
+        int amount = 999;
+        int expected = 1;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals (actual,expected);
+    }
+
+    @Test
+    public void ShouldBuy1000USD() {
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals (actual,expected);
+    }
+
+    @Test
+    public void ShouldBuy1200USD() {
+        int amount = 1200;
+        int expected = 800;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals (actual,expected);
+    }
+
+
+
+
+}
